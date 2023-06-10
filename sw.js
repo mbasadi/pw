089744d988755097f19f7b07c0cba88c80767251
mbasadi <asadi.mohammadbagher@gmail.com>
2023-06-10 12:50:28 -0400

    new sw

--- sw.js
+++ sw.js
@@ -1,21 +1,23 @@
-self.addEventListener('push', function(e) {
-  console.log('e:',e)
-  var options = {
-    body: 'This notification was generated from a push!',
-    icon: 'images/example.png',
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: '2'
-    },
-    actions: [
-      {
-        action: 'explore',
-        title: 'Explore this new world',
-        icon: 'images/checkmark.png'
-      },
-      { action: 'close', title: 'Close', icon: 'images/xmark.png' }
-    ]
-  };
-  e.waitUntil(self.registration.showNotification('Hello world!', options));
+self.addEventListener("push", (event) => {
+  if (!(self.Notification && self.Notification.permission === "granted")) {
+    return;
+  }
+
+  const data = event.data?.json() ?? {};
+  const title = data.title || "Something Has Happened";
+  const message =
+    data.message || "Here's something you might want to check out.";
+  const icon = "images/new-notification.png";
+
+  const notification = new self.Notification(title, {
+    body: message,
+    tag: "simple-push-demo-notification",
+    icon,
+  });
+
+  notification.addEventListener("click", () => {
+    clients.openWindow(
+      "https://developer.mozilla.org/en-US/docs/Web/API/PushEvent"
+    );
+  });
 });
