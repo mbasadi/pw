fa305f72c6cb1eecf659abb15e3722ddeb4c2eb5
mbasadi <asadi.mohammadbagher@gmail.com>
2023-06-29 17:07:30 -0400

    url on click

--- sw.js
+++ sw.js
@@ -2,21 +2,15 @@ self.addEventListener('push', (event) => {
   if (!(self.Notification && self.Notification.permission === 'granted')) {
     return;
   }
-console.log(event)
+
+  console.log(event);
+
   const data = event.data?.json() ?? {};
   const title = data.title || 'Something Has Happened';
   const message = data.message || 'Here\'s something you might want to check out.';
   const icon = data.icon||'images/new-notification.png';
   const tag = data.tag||'simple-push-Tag';
-  const clickURL=data.url||'https://datatracker.ietf.org/doc/html/rfc8292#section-2.1'
-
-  // Log the URL of each client this service worker controls
-  clients.matchAll({ includeUncontrolled: true, type: 'window' })
-    .then(windowClients => {
-      windowClients.forEach(windowClient => {
-        console.log(windowClient.url);
-      });
-    });
+  const clickURL=data.url||'https://datatracker.ietf.org/doc/html/rfc8292#section-2.1';
 
   event.waitUntil(
     self.registration.showNotification(title, {
@@ -29,26 +23,16 @@ console.log(event)
       body: message,
       tag,
       icon,
+      data: { url: clickURL }, // Add this line to store the clickURL
     })
   );
-  event.waitUntil(
-    console.log(title)
-  )
 });
 
 self.addEventListener('notificationclick', function (event) {
   event.notification.close();
-  console.log('event',event)
-  console.log('event.action',event.action)
-  // Log the URL of each client this service worker controls
-  clients.matchAll({ includeUncontrolled: true, type: 'window' })
-    .then(windowClients => {
-      windowClients.forEach(windowClient => {
-        console.log(windowClient.url);
-      });
-    });
+  const clickURL = event.notification.data.url; // Retrieve the URL from the notification data
 
   event.waitUntil(
-    clients.openWindow('/')
+    clients.openWindow(clickURL) // Open the clickURL when notification is clicked
   );
 });
